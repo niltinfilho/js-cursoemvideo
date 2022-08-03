a307ccd0211b21f573bc6f7908647f162a920225
Nilton Filho <niltonfilho115@gmail.com>
2022-08-03 20:37:37 -0300

    fazendo exercicios

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,25 @@
-alert('Olá, mundo!')
+function carregar() {
+    var msg = document.querySelector('#msg')
+    var img = document.querySelector('#imagem')
+
+    var data = new Date()
+    // var hora = data.getHours()
+    var hora = 7
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 6 && hora < 12) {
+         // bom dia
+         img.src = 'foto-manha.png'
+         document.body.style.background = '#E7DBC2'
+
+    } else if (hora >= 12 && hora < 18) {
+        // boa tarde
+        img.src = 'foto-tarde.png'
+        document.body.style.background = '#B9846F'
+    } else {
+        // boa noite
+        img.src = 'foto-noite.png'
+        document.body.style.background = '#113C55'
+    }
+}
