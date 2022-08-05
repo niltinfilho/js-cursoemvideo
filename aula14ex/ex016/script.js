1d0b111531ff01e0a10dac65a82157198e75c6e2
Nilton Filho <niltonfilho115@gmail.com>
2022-08-04 23:10:53 -0300

    aa

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -8,12 +8,18 @@ function contar() {
     let passo = document.querySelector('#txtp');
     let p = Number(passo.value)
 
+    if (p <= 0) {
+        p = 1
+        alert('Passo inválido! Considerando passo 1')
+    }
+
     let res = document.querySelector('#res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando: <br>'
 
         if (i < f) {
             
@@ -29,6 +35,5 @@ function contar() {
         }
 
         res.innerHTML += `\u{1F3C1}`
-        
     }
 }
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,22 @@
-alert('Olá, mundo!')
+function tabuada() {
+    let num = document.querySelector('#txtn')
+    let tab = document.querySelector('#seltab')
+
+    if (num.value.length != 0) {
+        let n = Number(num.value)
+
+        tab.innerHTML = ''
+
+        cont = 1
+        while (cont <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${cont} = ${n * cont}`
+            item.value = `tab${cont}`
+            tab.appendChild(item)
+
+            cont++
+        }
+    } else {
+        alert('Por favor, digite um número!')        
+    }
+} 
